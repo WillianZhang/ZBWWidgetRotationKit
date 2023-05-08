86d40768ef444e207bbfbf59d376ccb3a255f51a
张斌伟 <810016427@qq.com>
2023-05-08 15:39:51 +0800

    桌面旋转组件实现~

---
+++ ZBWWidgetRotationKit.framework/Headers/ZBWWidgetRotationKit.h
@@ -0,0 +1,18 @@
+//
+//  ZBWWidgetRotationKit.h
+//  ZBWWidgetRotationKit
+//
+//  Created by 张斌伟 on 2023/2/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZBWWidgetRotationKit.
+FOUNDATION_EXPORT double ZBWWidgetRotationKitVersionNumber;
+
+//! Project version string for ZBWWidgetRotationKit.
+FOUNDATION_EXPORT const unsigned char ZBWWidgetRotationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZBWWidgetRotationKit/PublicHeader.h>
+
+
